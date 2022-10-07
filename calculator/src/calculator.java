2671804c71e6e326ea7bad3f911d7b794f40c1b8
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2022-10-07 16:29:07 +0100

    completed the addition method

--- calculator/src/calculator.java
+++ calculator/src/calculator.java
@@ -4,10 +4,11 @@ public class calculator{
 
     /*
      * Addition method
+     * @return the sum of x plus y
      */
 
      public int addition(int x, int y){
-        return -1;
+        return x + y;
      }
 
 }
