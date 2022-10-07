16698b1564e35879ca2a12a046609db6cad53e7b
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2022-10-07 17:00:05 +0100

    Started to implement the user into the program

--- calculator/src/Main.java
+++ calculator/src/Main.java
@@ -1,13 +1,26 @@
 package calculator.src;
-// import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
         
-        calculator c = new calculator();
-        c.addition(10, 1);
-        c.subtraction(10, 1);
-        c.multiplication(5, 15);
-        c.division(15, 5);
+        // calculator c = new calculator();
+        // c.addition(10, 1);
+        // c.subtraction(10, 1);
+        // c.multiplication(5, 15);
+        // c.division(5, 37);
+
+        System.out.println("WELCOME TO THE CALCULATOR");
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter x: ");
+        int x = scan.nextInt();
+
+        System.out.println("Enter y: ");
+        int y = scan.nextInt();
+
+        
+
     }
 }
